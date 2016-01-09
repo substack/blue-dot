ae2558c5bfcdc9eaaa601d77552b347ed8a79cb7
James Halliday <mail@substack.net>
2016-01-09 18:09:26 -0500

    cache complexes from the mesh state

--- main.js
+++ main.js
@@ -3,14 +3,56 @@ var glclear = require('gl-clear')
 var glBuffer = require('gl-buffer')
 var complex = require('gl-simplicial-complex')
 var sphere = require('primitive-sphere')
+var shp2json = require('shapefile2geojson')
+var Zip = require('zip')
 
 var gecef = require('geodetic-to-ecef')
 var wgs84 = require('wgs84')
 var xtend = require('xtend')
 
+var dragDrop = require('drag-and-drop-files')
+dragDrop(window, function (files) {
+  files.forEach(function (file) {
+    if (/\.zip$/i.test(file.name)) {
+      var reader = new FileReader();
+      reader.addEventListener('load', function (ev) {
+        var buf = Buffer(new Uint8Array(ev.target.result))
+        loadZip(file, buf)
+      })
+      reader.readAsArrayBuffer(file)
+    }
+  })
+})
+
+function loadZip (file, buf) {
+  var z = new Zip.Reader(buf)
+  var dbf, shp
+  z.forEach(function (entry) {
+    if (!entry.isFile()) return
+    var name = entry.getName()
+    if (/\.dbf$/i.test(name)) {
+      dbf = entry
+    } else if (/\.shp$/i.test(name)) {
+      shp = entry
+    }
+  })
+  if (dbf && shp) {
+    var data = shp2json(shp.getData(), dbf.getData())
+    addGeoJSON(data)
+  }
+}
+
+function addGeoJSON (data) {
+  loop.state.overlays.push({ type: 'geojson', data: data })
+  loop.update(loop.state)
+}
+
 var clear = glclear({ color: [0.15, 0.08, 0.10, 1.0] })
 var vdom = require('virtual-dom')
 var h = require('virtual-hyperscript-hook')(vdom.h)
+
+var gl, complexes = {}, meshCache = {}
+
 var main = require('main-loop')
 var loop = main({
   width: window.innerWidth,
@@ -21,7 +63,8 @@ var loop = main({
       return mat4.translate(v, v, [0,0,-3*wgs84.RADIUS/1e3])
     })(),
     rotation: mat4.create()
-  }
+  },
+  meshes: { earth: createEarth() }
 }, render, vdom)
 document.querySelector('#content').appendChild(loop.target)
 
@@ -43,15 +86,14 @@ function render (state) {
       height: state.height,
       hook: function (canvas) {
         gl = canvas.getContext('webgl')
-        ongl(gl)
+        ongl(gl, state)
         draw(gl, state)
       }
     })
   ])
 }
 
-var gl, earth
-function ongl (gl) {
+function createEarth () {
   var pos = [], cells = [], colors = [], normals = []
   pos.push([0,0,0])
   colors.push([0,1,1])
@@ -69,17 +111,21 @@ function ongl (gl) {
     normals.push([0,0,-1])
     normals.push([0,0,-1])
   }
-  var edges = []
-  cells.forEach(function (cell) {
-    edges.push([cell[0],cell[1]])
-    edges.push([cell[1],cell[2]])
-    edges.push([cell[2],cell[0]])
-  }),
-  earth = complex(gl, {
+  return {
     cells: cells,
     vertexColors: colors,
     vertexNormals: normals,
     positions: pos
+  }
+}
+
+function ongl (gl, state) {
+  complexes = {}
+  Object.keys(state.meshes).forEach(function (key) {
+    if (meshCache[key] !== state.meshes[key]) {
+      complexes[key] = complex(gl, state.meshes[key])
+      meshCache[key] = state.meshes[key]
+    }
   })
 }
 
@@ -91,10 +137,13 @@ function draw (gl, state) {
   var view = mat4.create()
   mat4.multiply(view, view, state.camera.rotation)
   mat4.multiply(view, view, state.camera.translation)
-  earth.draw({
-    view: view,
-    projection: mat4.perspective(
-      mat4.create(), Math.PI/4.0, width/height, 0.1, 1e10)
+
+  Object.keys(complexes).forEach(function (key) {
+    complexes[key].draw({
+      view: view,
+      projection: mat4.perspective(
+        mat4.create(), Math.PI/4.0, width/height, 0.1, 1e10)
+    })
   })
 }
 
