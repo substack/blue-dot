50036bb6df1e879743c8405357111a076e0a3513
James Halliday <mail@substack.net>
2016-01-10 04:18:03 -0500

    fixed scroll wheel and maximum zoom

--- main.js
+++ main.js
@@ -108,7 +108,7 @@ var main = require('main-loop')
 var loop = main({
   width: window.innerWidth,
   height: window.innerHeight,
-  camera: [25,-120, wgs84.RADIUS*3],
+  camera: [25,-120, wgs84.RADIUS*2],
   meshes: { earth: createEarth() }
 }, render, vdom)
 document.querySelector('#content').appendChild(loop.target)
@@ -132,11 +132,11 @@ window.addEventListener('keydown', function (ev) {
     loop.update(xtend(loop.state, { camera: [c[0]-1,c[1],c[2]] }))
   } else if (ev.keyCode === 187) { // -
     loop.update(xtend(loop.state, {
-      camera: [c[0],c[1],wgs84.RADIUS+(c[2]-wgs84.RADIUS)/1.1]
+      camera: [c[0],c[1],Math.max(1e3,c[2]/1.1)]
     }))
   } else if (ev.keyCode === 189) { // +
     loop.update(xtend(loop.state, {
-      camera: [c[0],c[1],wgs84.RADIUS+(c[2]-wgs84.RADIUS)*1.1]
+      camera: [c[0],c[1],Math.min(wgs84.RADIUS*4,c[2]*1.1)]
     }))
   }
   console.log(ev.keyCode)
@@ -144,7 +144,8 @@ window.addEventListener('keydown', function (ev) {
 
 window.addEventListener('wheel', function (ev) {
   var c = loop.state.camera
-  var z = wgs84.RADIUS+(c[2]-wgs84.RADIUS)*Math.pow(1.1,ev.deltaY/100)
+  var z = Math.max(1e3,Math.min(
+    wgs84.RADIUS*4, c[2]*(1+ev.deltaY/1000)))
   loop.update(xtend(loop.state, { camera: [c[0],c[1],z] }))
 })
 
@@ -214,7 +215,11 @@ function draw (gl, state) {
   gl.viewport(0, 0, width, height)
   gl.enable(gl.DEPTH_TEST)
   var camera = lookat()
-  camera.position = xecef(state.camera[0], state.camera[1], state.camera[2])
+  camera.position = xecef(
+    state.camera[0],
+    state.camera[1],
+    state.camera[2]
+  )
   camera.up = [0,0,1]
   camera.target = [0,0,0]
 
