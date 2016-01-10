38344728e48f01856ae559ba84d391406c1bf76b
James Halliday <mail@substack.net>
2016-01-10 03:47:44 -0500

    zoom

--- main.js
+++ main.js
@@ -116,22 +116,29 @@ window.addEventListener('resize', function () {
 window.addEventListener('keydown', function (ev) {
   var c = loop.state.camera
   if (ev.keyCode === 37) { // left
-    loop.update(xtend(loop.state, {
-      camera: [c[0],c[1]-1,c[2]]
-    }))
+    loop.update(xtend(loop.state, { camera: [c[0],c[1]-1,c[2]] }))
   } else if (ev.keyCode === 39) { // right
-    loop.update(xtend(loop.state, {
-      camera: [c[0],c[1]+1,c[2]]
-    }))
+    loop.update(xtend(loop.state, { camera: [c[0],c[1]+1,c[2]] }))
   } else if (ev.keyCode === 38) { // up
-    loop.update(xtend(loop.state, {
-      camera: [c[0]+1,c[1],c[2]]
-    }))
+    loop.update(xtend(loop.state, { camera: [c[0]+1,c[1],c[2]] }))
   } else if (ev.keyCode === 40) { // down
+    loop.update(xtend(loop.state, { camera: [c[0]-1,c[1],c[2]] }))
+  } else if (ev.keyCode === 187) { // -
     loop.update(xtend(loop.state, {
-      camera: [c[0]-1,c[1],c[2]]
+      camera: [c[0],c[1],wgs84.RADIUS+(c[2]-wgs84.RADIUS)/1.1]
+    }))
+  } else if (ev.keyCode === 189) { // +
+    loop.update(xtend(loop.state, {
+      camera: [c[0],c[1],wgs84.RADIUS+(c[2]-wgs84.RADIUS)*1.1]
     }))
   }
+  console.log(ev.keyCode)
+})
+
+window.addEventListener('wheel', function (ev) {
+  var c = loop.state.camera
+  var z = wgs84.RADIUS+(c[2]-wgs84.RADIUS)*Math.pow(1.1,ev.deltaY/100)
+  loop.update(xtend(loop.state, { camera: [c[0],c[1],z] }))
 })
 
 function render (state) {
